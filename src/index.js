8eea39a8cd4dcfec17c76973e5d22b6d49bb5ab1
alexisteh <wenhui.teh23@gmail.com>
2020-07-15 12:21:45 -0400

    done

--- src/index.js
+++ src/index.js
@@ -8,14 +8,18 @@ document.addEventListener('DOMContentLoaded', function(){
         return document.createElement(element) 
     }
 
-    // grab useful objects from DOM 
+    // grabs useful objects from DOM 
     const characterBar = qs('div#character-bar')
-    const nameArea = qs('p#name')
+    const nameArea = qs('p#name') 
     const imgArea = qs('img#image')
     const caloriesArea = qs('span#calories') 
     const caloriesForm = qs('form#calories-form')
     const resetCaloriesButton = qs('button#reset-btn')
+
     const newNameForm = qs('form#new-name-form')
+    const newCharacterForm = qs('form#new-character-form') 
+    const newCharacterNameInput = qs('input#new-character-name')
+    const newCharacterImageInput = qs('input#new-character-image')
 
     // fetch all characters 
     function fetchCharacters(){
@@ -30,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     // adds a character to the top bar  
+    // re-use for making new character  
     function addCharacter(character){
         const characterSpan = ce('span') 
         characterSpan.innerText = character.name 
@@ -60,12 +65,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
             const currentCalories = parseInt(caloriesArea.innerText)
             let caloriesToAdd = qs('input#added-calories').value 
-            // in case user leaves the field blank 
-            if (caloriesToAdd == ""){
+            // in case user leaves the field blank or types in nonsense  
+            const numbers = /^[0-9]+$/ 
+            if (!caloriesToAdd.match(numbers)){
                 caloriesToAdd = "0" 
             } 
             const updatedCalories = currentCalories + parseInt(caloriesToAdd) 
-
             const configObj = { 
                 method: 'PATCH',
                 headers: { 
@@ -80,37 +85,35 @@ document.addEventListener('DOMContentLoaded', function(){
                 .then(res => res.json())
                 .then(updatedCharacter => {
                     caloriesArea.innerText = updatedCharacter.calories 
-                    caloriesForm.reset() 
                 }) 
-        } 
+        }
+        caloriesForm.reset()  
     })
 
 
     // updates DB and DOM for reset calories  
-    resetCaloriesButton.addEventListener('click', function(){
-
+    resetCaloriesButton.addEventListener('click', function(){ 
         // conditional in case a character was not selected   
         if (nameArea.innerText != "Character's Name") { 
-        const slimmedCharacterId = qs('input[type="hidden"]').id  
-
-        const configObj = { 
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({ 
-                calories: 0 
-            }) 
-        }  
-        fetch('http://localhost:3000/characters/'+ slimmedCharacterId, configObj)
-            .then(res => res.json())
-            .then(slimmedCharacter => { 
-                caloriesArea.innerText = "0" 
-            }) 
+            const slimmedCharacterId = qs('input[type="hidden"]').id  
+            const configObj = { 
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    calories: 0 
+                }) 
+            }  
+            fetch('http://localhost:3000/characters/'+ slimmedCharacterId, configObj)
+                .then(res => res.json())
+                .then(slimmedCharacter => { 
+                    caloriesArea.innerText = "0" 
+                }) 
     }}) 
 
-    // updates DB and DOM for updating name   
+    // updates DB and DOM for changing name   
     newNameForm.addEventListener('submit', function(){
         event.preventDefault()
         // conditional in case a character was not selected   
@@ -137,11 +140,37 @@ document.addEventListener('DOMContentLoaded', function(){
                     nameArea.innerText = renamedCharacter.name 
                     qs('span#character-'+renamedCharacter.id).innerText = renamedCharacter.name
                 })
-            newNameForm.reset() 
-        } 
+        }
+        newNameForm.reset()  
     })
 
+    // updates DB and DOM for making new character    
+    newCharacterForm.addEventListener('submit', function(){
+        event.preventDefault()
+        const newCharacterName = newCharacterNameInput.value
+        const newCharacterImage = newCharacterImageInput.value
+        const configObj = { 
+            method: 'POST', 
+            headers: { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({ 
+                name: newCharacterName, 
+                image: newCharacterImage,
+                calories: 0
+            }) 
+        } 
+        fetch("http://localhost:3000/characters", configObj)
+            .then(res => res.json())
+            .then(newCharacter => {
+                addCharacter(newCharacter)
+                console.log(newCharacter)
+            })  
+        newCharacterForm.reset() 
+    })
 
+    // call function to begin fetching characters, setting up page, on DOM load    
     fetchCharacters()
 
 })
